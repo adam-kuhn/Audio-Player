288d8773f6e3400546d848defa4dd7559b78201b
adam-kuhn <adam.kuhn20@gmail.com>
2018-05-03 16:06:57 +1200

    local file playing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,7 @@ import {HashRouter as Router} from 'react-router-dom'
 
 import MusicList from './MusicList'
 import Details from './Details'
+import ReactAudioPlayer from 'react-audio-player'
 
 const App = () => {
   return (
@@ -11,6 +12,10 @@ const App = () => {
         <h2>React and Redux development is ready to GOOOO!</h2>
         <MusicList />
         <Details />
+        <ReactAudioPlayer src='/sounds/Delta1.mp3'
+          autoPlay
+          controls
+        />
       </div>
     </Router>
   )
