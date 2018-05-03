c1799c0f0ab29d0aedb257deaad319cb2436cecb
adam-kuhn <adam.kuhn20@gmail.com>
2018-05-03 20:34:43 +1200

    rmv default audio file

--- client/components/Player.jsx
+++ client/components/Player.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import JPlayer, {initializeOptions, status, actions, Gui, SeekBar, BufferBar,
+import JPlayer, {initializeOptions, actions, Gui, SeekBar, BufferBar,
   Audio, Title, Mute, Play, PlayBar,
   VolumeBar, Duration, CurrentTime, BrowserUnsupported
 } from 'react-jplayer'
@@ -8,17 +8,17 @@ import JPlayer, {initializeOptions, status, actions, Gui, SeekBar, BufferBar,
 // import winston from '/music/winston.mp3'
 
 const defaultOptions = {
-  id: 'AudioPlayer',
-  keyEnabled: true,
-  verticalVolume: true,
-  media: {
-    title: 'Winston Tastes Good Like A Cigarette Should Jingle',
-    // artist: 'Miaow',
-    sources: {
-      mp3: '/music/winston.mp3'
-    },
-    free: true
-  }
+  id: 'AudioPlayer'
+  // keyEnabled: true,
+  // verticalVolume: true,
+  // media: {
+  //   title: 'Winston Tastes Good Like A Cigarette Should Jingle',
+  //   // artist: 'Miaow',
+  //   sources: {
+  //     mp3: '/music/winston.mp3'
+  //   },
+  //   free: true
+  // }
 }
 initializeOptions(defaultOptions)
 class Player extends React.Component {
