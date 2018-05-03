f32ad18df0821b1e45f04aaae8501f1b412c77fc
adam-kuhn <adam.kuhn20@gmail.com>
2018-05-03 20:15:48 +1200

    skip ahead and back by 15 seconds;

--- client/components/Player.jsx
+++ client/components/Player.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import JPlayer, {initializeOptions, Gui, SeekBar, BufferBar,
+import JPlayer, {initializeOptions, status, actions, Gui, SeekBar, BufferBar,
   Audio, Title, Mute, Play, PlayBar,
   VolumeBar, Duration, CurrentTime, BrowserUnsupported
 } from 'react-jplayer'
@@ -24,6 +24,16 @@ initializeOptions(defaultOptions)
 class Player extends React.Component {
   constructor () {
     super()
+    this.skipBack = this.skipBack.bind(this)
+    this.skipAhead = this.skipAhead.bind(this)
+  }
+  skipBack () {
+    const rwd15 = this.props.currentTime - 15
+    this.props.dispatch(actions.play('AudioPlayer', rwd15))
+  }
+  skipAhead () {
+    const ffw15 = this.props.currentTime + 15
+    this.props.dispatch(actions.play('AudioPlayer', ffw15))
   }
   render () {
     return (
@@ -31,7 +41,9 @@ class Player extends React.Component {
         <Audio />
         <Gui>
           <div className="jp-controls jp-icon-controls">
+            <button type='button' onClick={this.skipBack}>RWD</button>
             <Play><i className="fa">{/* Icon set in css */}Play</i></Play>
+            <button type='button' onClick={this.skipAhead}>FFW</button>
             <div className="jp-progress">
               <SeekBar>
                 <BufferBar />
@@ -63,7 +75,7 @@ class Player extends React.Component {
 
 function mapStateToProps (state) {
   return {
-    // showRemainingDuration: state.jPlayers.AudioPlayer.showRemainingDuration
+    currentTime: state.jPlayers.AudioPlayer.currentTime
   }
 }
 
