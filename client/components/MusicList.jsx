307a64b7510ea4a8be97c73a09c2aabedd34766d
adam-kuhn <adam.kuhn20@gmail.com>
2018-05-04 10:00:58 +1200

    toggle sorting of song list

--- client/components/MusicList.jsx
+++ client/components/MusicList.jsx
@@ -4,13 +4,18 @@ import {Link} from 'react-router-dom'
 import {withRouter} from 'react-router'
 import {actions} from 'react-jplayer'
 
-import music from '../../data.json'
 import {selectSong} from '../actions'
+import music from '../../data.json'
 
 class MusicList extends React.Component {
   constructor () {
     super()
+    this.state = {
+      aToZ: true,
+      musicList: music
+    }
     this.handleClick = this.handleClick.bind(this)
+    this.sortList = this.sortList.bind(this)
   }
 
   handleClick (evt) {
@@ -28,19 +33,55 @@ class MusicList extends React.Component {
     this.props.dispatch(actions.play('AudioPlayer'))
   }
 
+  sortList () {
+    if (this.state.aToZ === true) {
+      this.setState({
+        aToZ: false
+      })
+    } else {
+      this.setState({
+        aToZ: true
+      })
+    }
+  }
+
   render () {
+    if (this.state.aToZ === true) {
+      this.state.musicList.sort((a, b) => {
+        if (a.title < b.title) {
+          return -1
+        }
+        if (a.title > b.title) {
+          return 1
+        }
+        return 0
+      })
+    } else {
+      this.state.musicList.sort((a, b) => {
+        if (a.title > b.title) {
+          return -1
+        }
+        if (a.title < b.title) {
+          return 1
+        }
+        return 0
+      })
+    }
     return (
-      <ul>
-        {music.map((song, idx) => {
-          return (
-            <li key={idx}>
-              <Link to='#' onClick={this.handleClick}
-                data-title={song.title} data-file={song.file}
-                data-description={song.description}>{song.title}</Link>
-            </li>
-          )
-        })}
-      </ul>
+      <div>
+        <button type='button' onClick={this.sortList}>Sort</button>
+        <ul>
+          {this.state.musicList.map((song, idx) => {
+            return (
+              <li key={idx}>
+                <Link to='#' onClick={this.handleClick}
+                  data-title={song.title} data-file={song.file}
+                  data-description={song.description}>{song.title}</Link>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     )
   }
 }
