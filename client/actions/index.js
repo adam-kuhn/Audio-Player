38e2709a6c39a7e851a9e05dc7fdaac6b5256778
adam-kuhn <adam.kuhn20@gmail.com>
2018-05-03 12:09:06 +1200

    display song details

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,7 +1,6 @@
 export const SELECT_SONG = 'SELECT_SONG'
 
 export const selectSong = (title, description, file) => {
-  console.log(title, description, file)
   return {
     type: SELECT_SONG,
     title,
