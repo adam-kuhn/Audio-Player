54e43eb597693dcf35dac464310d24f73ccd7640
adam-kuhn <adam.kuhn20@gmail.com>
2018-05-04 21:00:18 +1200

    reducer passing test

--- test/reducer.test.js
+++ test/reducer.test.js
@@ -1,2 +1,19 @@
-const music = require('../client/reducers/music')
-const 
+import music from '../client/reducers/music'
+import {SELECT_SONG} from '../client/actions'
+
+const initialState = {
+  title: '',
+  description: '',
+  file: ''
+}
+
+test('music reducer return correct state', () => {
+  const action = {
+    type: SELECT_SONG,
+    title: 'song title',
+    description: 'song description',
+    file: 'song file'
+  }
+  const newState = music(initialState, action)
+  expect(newState.title).toBe(action.title)
+})
