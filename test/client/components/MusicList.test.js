d0cb5db79f97aa8d899f5e2e14b2b2d1b4f7ca88
adam-kuhn <adam.kuhn20@gmail.com>
2018-05-06 15:19:29 +1200

    musiclist component test passing

--- test/client/components/MusicList.test.js
+++ test/client/components/MusicList.test.js
@@ -2,12 +2,13 @@ import React from 'react'
 import {render} from 'enzyme'
 import {Provider} from 'react-redux'
 import configureStore from 'redux-mock-store'
+import {HashRouter as Router} from 'react-router-dom'
 
 import MusicList from '../../../client/components/MusicList'
 
 afterEach(jest.resetModules)
 
-test('<MusicList /> renders header', () => {
+test('<MusicList /> renders h2', () => {
   const mockStore = configureStore()({
     music: {
       tite: 'song title',
@@ -17,7 +18,26 @@ test('<MusicList /> renders header', () => {
   })
   const wrapper = render(
     <Provider store={mockStore}>
-      <MusicList />
+      <Router>
+        <MusicList />
+      </Router>
     </Provider>)
   expect(wrapper.find('h2').length).toBe(1)
+})
+
+test('<MusicList /> renders list of songs', () => {
+  const mockStore = configureStore()({
+    music: {
+      tite: 'song title',
+      description: 'song description',
+      file: 'song file'
+    }
+  })
+  const wrapper = render(
+    <Provider store={mockStore}>
+      <Router>
+        <MusicList/>
+      </Router>
+    </Provider>)
+  expect(wrapper.find('h3').length).toBe(7)
 })
